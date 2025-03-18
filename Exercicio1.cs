db40a6d40279eb72d0878a20b99073f86b9b6512
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-17 21:18:08 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[15];
+
+            Console.WriteLine("Escreva 15 números inteiros:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Os números que estão armazenados nas posições pares são:");
+            for (int i = 0; i < 15; i += 2)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
