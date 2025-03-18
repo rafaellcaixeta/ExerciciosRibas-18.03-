d245d4fdb95fbced139cd5af310210ba1b86da72
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-17 21:20:44 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5, 5];
+            int valor = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matriz[i, j] = valor;
+                    valor++;
+                }
+            }
+
+            Console.WriteLine("Valores das diagonais da matriz 5x5:\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j || i + j == 4)
+                    {
+                        Console.WriteLine(matriz[i, j] + "\t");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
